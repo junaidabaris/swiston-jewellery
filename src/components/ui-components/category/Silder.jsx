9764e0ec387accb82ee715f55636a126af457935
Mohd Nizam <mohdnizam251099@gmail.com>
2024-01-20 18:20:23 +0530

    details page

--- src/components/ui-components/category/Silder.jsx
+++ src/components/ui-components/category/Silder.jsx
@@ -2,48 +2,49 @@ import React from "react";
 import { GrFormPrevious } from "react-icons/gr";
 import { MdOutlineNavigateNext } from "react-icons/md";
 import Slider from "react-slick";
-
-export default function Silder({item}){
-    function SampleNextArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-          <div
-            className="next"
-            onClick={onClick}>
-            <MdOutlineNavigateNext />
-          </div>
-        );
-      }
-      function SamplePrevArrow(props) {
-        const { className, style, onClick } = props;
-        return (
-          <div
-            className="prev"
-            onClick={onClick}>
-                <GrFormPrevious /></div>
-    
-        );
-      }
-    const settings = {
-        dots: false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        nextArrow: <SampleNextArrow />,
-        prevArrow: <SamplePrevArrow />
-      };
-    return(
-        <>
-         <div className="silder">
-         <Slider {...settings} className="slider_box">
-            {item?.img1 && item?.img1?.map((item) => {
-  return <div className="card_img" key={item?.id}>
-  <img src={item.img} alt="ring"/>
-  </div>
+import { Link } from "react-router-dom";
+export default function Silder({ item }) {
+  function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className="next" onClick={onClick}>
+        <MdOutlineNavigateNext />
+      </div>
+    );
+  }
+  function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div className="prev" onClick={onClick}>
+        <GrFormPrevious />
+      </div>
+    );
+  }
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <>
+      <div className="silder">
+        <Slider {...settings} className="slider_box">
+          {item?.img1 &&
+            item?.img1?.map((item) => {
+              return (
+                <div className="card_img" key={item?.id}>
+                  <Link to="/product-details">
+                    {" "}
+                    <img src={item.img} alt="ring" />
+                  </Link>
+                </div>
+              );
             })}
-                
-                
-                {/* <div>
+
+          {/* <div>
                     <img src={item.img2} alt="ring" />
                 </div>
                 <div>
@@ -55,8 +56,8 @@ export default function Silder({item}){
                 <div>
                     <img src={item.img5} alt="ring" />
                 </div> */}
-            </Slider>
-         </div>
-        </>
-    )
+        </Slider>
+      </div>
+    </>
+  );
 }
