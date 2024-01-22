e59c4bdc992cdfb17eef2dcc1d72c721484a9098
junaid <junaid.abaris@gmail.com>
2024-01-22 19:47:56 +0530

    live

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
     </Provider>
-    {/* <UIApp /> */}
+    <UIApp />
   </React.StrictMode>
 );
 
