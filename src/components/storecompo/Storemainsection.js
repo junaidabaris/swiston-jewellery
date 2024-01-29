c91582a3f00fa3c9d288d4ef0f1141772263ffd0
junaid <junaid.abaris@gmail.com>
2024-01-29 15:06:42 +0530

    chaneges

--- src/components/storecompo/Storemainsection.js
+++ src/components/storecompo/Storemainsection.js
@@ -4,7 +4,7 @@ export const Storemainsection = () => {
     return <div className="store-main">
         <div className="">
             <div className="card-holder">
-                <p className="cardpera">Showing 1 Stores in <span>Delhi</span></p>
+                <p className="cardpera">Showing 5 Stores in <span>Delhi</span></p>
                 <div className="carslists">
                     <CardsStore />
                     <CardsStore />
