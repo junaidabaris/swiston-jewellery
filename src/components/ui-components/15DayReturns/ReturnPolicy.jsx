0515a1fd599fa317ca6b1463bbb103235f91115b
aqibnezami <aqibnezami@gmail.com>
2024-02-03 19:04:39 +0530

    logo change

--- src/components/ui-components/15DayReturns/ReturnPolicy.jsx
+++ src/components/ui-components/15DayReturns/ReturnPolicy.jsx
@@ -8,7 +8,7 @@ export default function Returnpolicy(){
     return(
         <div className="main_Returnpolicy">
             <div className="main_Returnpolicy_header">
-                <h1>ReturnPolicy</h1>
+                <h1>Return Policy</h1>
                 <p>Go ahead and shop with complete peace of mind!</p>
             </div>
             <div><TermConditon /></div>
